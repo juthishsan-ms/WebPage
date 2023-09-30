d25afad0afc42977dbb794fe18147a75fcfec3ec
juthishsan-ms <juthishsanms.21bcr@kongu.edu>
2023-09-30 19:56:43 +0530

    modified account home register files

--- register.php
+++ register.php
@@ -1,22 +1,24 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-    $conn = mysqli_connect('localhost', 'root', ' ', 'paintstore_db') or die("Connection failed: " . mysqli_connect_error());
-
-    //if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['mail']) && isset($_POST['pass']) && isset($_POST['mob'])) {
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $email = $_POST['mail'];
-        $pass = $_POST['pass'];
-        $phno = $_POST['mob'];
-
-        $sql = "INSERT INTO registration (Firstname, Lastname, Email, Pass, Mobile) VALUES ('$fname', '$lname', '$email', '$pass', '$phno')"; // Removed single quotes around column names
-
-        $query = mysqli_query($conn, $sql);
+    if ($dbc = mysqli_connect('localhost', 'root', '', 'paintstore_db')) {
+        $fname = $_POST['finame'];
+        $lname = $_POST['laname'];
+        $email = $_POST['email'];
+        $pass = $_POST['passw'];
+        $phone = $_POST['mobile'];
+        $sql = "INSERT INTO register(fname, lname, mail, pass, phno) VALUES('$fname','$lname','$email','$pass', '$phone')";
+        $query = mysqli_query($dbc, $sql);
         if ($query) {
-            echo 'Entry Successful';
+            echo "Entry Successful";
         } else {
-            echo 'Error Occurred';
+            echo "Error in entry" . mysqli_error($dbc);
         }
+    } else {
+        die("Connection failed:" . mysqli_connect_error());
     }
-//}
+    mysqli_close($dbc);
+}
+else{
+    echo "connection failed";
+}
 ?>
