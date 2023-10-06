bdd35b0eb2197c2ee33dbb9240e5537eb41f38bf
juthishsan-ms <juthishsanms.21bcr@kongu.edu>
2023-10-06 13:09:24 +0530

    modified some files

--- Back-End/connectivity.js
+++ Back-End/connectivity.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8000;
 var mysql = require('mysql');
 
-app.use(express.static(path.join(__dirname, '..', 'Web Page')));
+app.use(express.static(path.join(__dirname, '..', 'Front-End')));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
 
 var conn = mysql.createConnection({
     host: "localhost",
@@ -22,18 +22,19 @@ conn.connect(function (err) {
 })
 
 app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '..', 'Web Page', 'account.html');
+    const filePath = path.join(__dirname, '..', 'Front-End', 'account.html');
     res.sendFile(filePath);
 });
 
-app.post('/submit', (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body);
     var sql = "insert into registration values(null,'"+req.body.fname+"','"+req.body.lname+"','"+req.body.email+"','"+req.body.pass+"',"+req.body.mobile+")";
     conn.query(sql, (err) => {
         if (err) throw err;
 
         console.log("Data saved");
-        res.render('index', { title: 'Data saved', message: 'Welcome to the world of mark antony maame' });
+        const filePath = path.join(__dirname, '..', 'Front-End', 'account.html');
+        res.sendFile(filePath);
 
         conn.end();
     });
